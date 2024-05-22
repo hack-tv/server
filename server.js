37d95fc2482ba92a599bce2bef29d9c1e17c7e28
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2024-05-22 10:53:27 +0700

    feat: setup socket connection for video call

--- server.js
+++ server.js
@@ -7,14 +7,30 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.use(cors());
 
 io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.emit('self', socket.id);
+
+  socket.on('call:start', ({ selfId, remoteId, signal }) => {
+    // console.log({ selfId, remoteId }, '<<< ini call:start');
+    io.to(remoteId).emit('call:started', { remoteId: selfId, signal });
+  });
+
+  socket.on('call:accept', ({ selfId, remoteId, signal }) => {
+    // console.log({ selfId, remoteId }, '<<< ini call:accept');
+    io.to(remoteId).emit('call:accepted', { remoteId: selfId, signal });
+  });
+
+  socket.on('call:end', ({ remoteId }) => {
+    // console.log({ remoteId }, '<<< ini call:end');
+    io.to(remoteId).emit('call:ended');
   });
 });
 
